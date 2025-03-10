8a1da9b49dd73be162c9dbacfd12feb9530bbcc0
Redon Jashari <redon.jasharip@gmail.com>
2025-03-10 14:52:23 +0100

    Homework solution for the fifth week

---
+++ hw5/problem5.1/FibonacciNumbers.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+using namespace chrono;
+
+int nthnFibonacciRecursion(int n) {
+    if (n <= 1)
+    {
+        return n;
+    }
+    
+    return nthnFibonacciRecursion(n - 1) + nthnFibonacciRecursion(n - 2);
+}
+
+int nthnFibonacciBottomup(int n) {
+    if (n <= 1)
+    {
+        return n;
+    }
+    
+    vector<int> dp(n + 1);
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; ++i)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+}
+
+int fibonacci_closed_form(int n) {
+    const double sqrt5 = sqrt(5);
+    const double phi = (1 + sqrt5) / 2;
+    const double psi = (1 - sqrt5) / 2;
+
+    return round((pow(phi, n) - pow(psi, n)) / sqrt5);
+}
+
+void multiply(vector<vector<int>>& mat1, vector<vector<int>>& mat2) {
+    int x = mat1[0][0] * mat2[0][0] + mat1[0][1] * mat2[1][0];
+    int y = mat1[0][0] * mat2[0][1] + mat1[0][1] * mat2[1][1];
+    int z = mat1[1][0] * mat2[0][0] + mat1[1][1] * mat2[1][0];
+    int w = mat1[1][0] * mat2[0][1] + mat1[1][1] * mat2[1][1];
+
+    mat1[0][0] = x;
+    mat1[0][1] = y;
+    mat1[1][0] = z;
+    mat1[1][1] = w;
+}
+
+void matrixPower(vector<vector<int>>& mat1, int n) {
+    if (n == 0 || n == 1) return;
+
+    vector<vector<int>> mat2 = {{1, 1}, {1, 0}};
+
+    matrixPower(mat1, n / 2);
+
+    multiply(mat1, mat1);
+
+    if (n % 2 != 0) {
+        multiply(mat1, mat2);
+    }
+}
+
+int nthFibonacciMatrix(int n) {
+    if (n <= 1) return n;
+
+    vector<vector<int>> mat1 = {{1, 1}, {1, 0}};
+
+    matrixPower(mat1, n - 1);
+
+    return mat1[0][0];
+}
+
+void measure_runtime() {
+    int test_values[] = {0, 1, 2, 3, 4, 5, 6, 8, 10, 13, 16, 20, 25, 32, 40};
+
+    cout << "n\tRecursive\tBottom-Up\tClosed-Form\tMatrix" << endl;
+    for (int n : test_values) {
+        auto start = high_resolution_clock::now();
+        nthnFibonacciRecursion(n);
+        auto end = high_resolution_clock::now();
+        auto time1 = duration_cast<microseconds>(end - start).count();
+
+        start = high_resolution_clock::now();
+        nthnFibonacciBottomup(n);
+        end = high_resolution_clock::now();
+        auto time2 = duration_cast<microseconds>(end - start).count();
+
+        start = high_resolution_clock::now();
+        fibonacci_closed_form(n);
+        end = high_resolution_clock::now();
+        auto time3 = duration_cast<microseconds>(end - start).count();
+
+        start = high_resolution_clock::now();
+        nthFibonacciMatrix(n);
+        end = high_resolution_clock::now();
+        auto time4 = duration_cast<microseconds>(end - start).count();
+
+        cout << n << "\t" << time1 << "us\t" << time2 << "us\t" << time3 << "us\t" << time4 << "us" << endl;
+    }
+}
+
+int main() {
+    measure_runtime();
+    return 0;
+}
+
+
+
+
+
---
+++ hw5/problem5.1/FibonacciPlt.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+
+n_values = [0, 1, 2, 3, 4, 5, 6, 8, 10, 13, 16, 20, 25, 32, 40]
+recursive_times = [0, 0, 0, 0, 0, 0, 0, 0, 1 , 4, 16, 111, 1264, 31452, 1169491]
+bottom_up_times = [0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 9, 2]
+closed_form_times = [11, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0]
+matrix_times = [0, 0, 3, 4, 3, 5, 5, 5, 7, 7, 8, 9, 10, 30, 5]
+
+plt.figure(figsize=(10, 6))
+plt.plot(n_values, recursive_times, label='Recursive')
+plt.plot(n_values, bottom_up_times, label='Bottom-Up')
+plt.plot(n_values, closed_form_times, label='Closed-Form')
+plt.plot(n_values, matrix_times, label='Matrix')
+
+plt.yscale("log")
+plt.xlabel("n")
+plt.ylabel("Execution Time (microseconds)")
+plt.title("Fibonacci Computation Time Comparison")
+plt.legend()
+plt.grid(True)
+plt.show()
